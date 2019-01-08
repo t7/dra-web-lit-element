463372745aa9ae0a2bb833ade157d882034b8917
Kien <kienvdao@gmail.com>
2019-01-08 11:32:59 -0500

    add svg wave

--- src/components/my-app.js
+++ src/components/my-app.js
@@ -1,27 +1,68 @@
 // Import the LitElement base class and html helper function
-import { LitElement, html } from '@polymer/lit-element';
+import {LitElement, html} from '@polymer/lit-element';
+import {waveSvg} from './my-svg'
 
 // Extend the LitElement base class
 class MyApp extends LitElement {
 
+  /**
+   * Implement `render` to define a template for your element.
+   *
+   * You must provide an implementation of `render` for any element
+   * that uses LitElement as a base class.
+   */
+  render() {
     /**
-     * Implement `render` to define a template for your element.
+     * `render` must return a lit-html `TemplateResult`.
      *
-     * You must provide an implementation of `render` for any element
-     * that uses LitElement as a base class.
+     * To create a `TemplateResult`, tag a JavaScript template literal
+     * with the `html` helper function:
      */
-    render(){
-        /**
-         * `render` must return a lit-html `TemplateResult`.
-         *
-         * To create a `TemplateResult`, tag a JavaScript template literal
-         * with the `html` helper function:
-         */
-        return html`
+    return html`
+      <style>
+        .current-weather {
+          background-color: black;
+          background-position: center;
+          background-repeat: no-repeat;
+          background-size: cover;
+          box-sizing: border-box;
+          color: white;
+          font-size: 1.5rem;
+          min-height: 50vh;
+          padding-bottom: 10vw;
+          position: relative;
+        }
+        
+        .current-weather:before {
+          background-color: rgba(0, 0, 0, 0.5);
+          content: '';
+          display: block;
+          height: 100%;
+          left: 0;
+          position: absolute;
+          top: 0;
+          width: 100vw;
+        }
+      
+        .current-weather__wave svg {
+          bottom: 0;
+          position: absolute;
+          width: 100%;
+          fill: #fff;
+        }
+  
+        .current-weather__wave svg path:first-child {
+          fill: #00adcf;
+        }
+      </style>
+
       <!-- template content -->
-      <p>A paragraph</p>
+      <section class="current-weather" style="background-image: url(&quot;https://d13k13wj6adfdf.cloudfront.net/urban_areas/boston_web-550bb970ba.jpg&quot;);">
+        <div class="current-weather__wave">${waveSvg}</div>
+      </section>
     `;
-    }
+  }
 }
+
 // Register the new element with the browser.
 customElements.define('my-app', MyApp);
---
+++ src/components/my-svg.js
@@ -0,0 +1,3 @@
+import { html } from '@polymer/lit-element';
+
+export const waveSvg = html`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 17"><path d="M0 30 V15 Q30 3 60 15 V30z" /><path d="M0 30 V12 Q30 17 55 12 T100 11 V30z" /></svg>`;
